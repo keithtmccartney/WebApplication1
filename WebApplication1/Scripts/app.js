c417073b4b9d2ae70cbe839b18f1675c869b1138
keithtmccartney <keithtmccartney@hotmail.com>
2017-02-11 02:29:53 +0000

    [Mithun Pattankar] ASP.NET MVC 5 – Using Angular 2 with TypeScript in Visual Studio
    Adding package.json to ASP.NET MVC 5; Configure to transpile TypeScript
    files; Using gulpfile.js to move files; Add TypeScript files for
    bootstrapping; Include systemjs.config.js to load modules; Change csHTML
    to load and render Angular 2.

---
+++ WebApplication1/Scripts/app.js
@@ -0,0 +1,31 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+const core_1 = require("@angular/core");
+let AppComponent = class AppComponent {
+    constructor() {
+        this.title = 'ASP.NET MVC 5 with Angular 2';
+        this.skills = ['MVC 5', 'Angular 2', 'TypeScript', 'Visual Studio 2015'];
+        this.myskills = this.skills[1];
+    }
+};
+AppComponent = __decorate([
+    core_1.Component({
+        selector: 'my-app',
+        template: `    
+    <h2>My favorite skill is: {{myskills}}</h2>
+    <p>Skill:</p>
+    <ul>
+      <li *ngFor="let skl of skills">
+        {{ skl }}
+      </li>
+    </ul>
+  `
+    })
+], AppComponent);
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.js.map
---
+++ WebApplication1/tsScripts/boot.ts
@@ -0,0 +1,11 @@
+﻿///<reference path="./../node_modules/angular2-in-memory-web-api/index.d.ts"/>
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
